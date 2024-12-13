c9b8b619584114348ce152f707dd782a2a39681f
ZachariaChaouchi <zachchaouchi@gmail.com>
2024-12-13 13:20:23 +0000

    ...

--- workspace/Ball.java
+++ workspace/Ball.java
@@ -1,13 +1,67 @@
 import java.awt.*;
 
 public class Ball {
-	//your code here!
-	
-	//don't forget you need instance variables:
-	
-	
-	//constructor(s):
-	
-	//methods:
+	// your code here!
 
+	// don't forget you need instance variables:
+	private int xpos;
+	private int ypos;
+	private int xVelocity;
+	private int yVelocity;
+	private int size;
+
+	// constructor(s):
+	public Ball(int initXPos, int initYPos, int initSize) {
+		this.xpos = initXPos;
+		this.ypos = initYPos;
+		this.size = initSize;
+		xVelocity = 1;
+		yVelocity = 1;
+	}
+	// methods:
+
+	public void move() {
+		this.xpos += this.xVelocity;
+		this.ypos += this.yVelocity;
+	}
+
+	public void draw(Graphics g) {
+		g.fillOval(xpos,ypos,size,size);
+	}
+
+	public int getXpos() {
+		return xpos;
+	}
+
+	public int getYpos() {
+		return ypos;
+	}
+
+	public int getSize() {
+		return size;
+	}
+
+	public void setXVelocity(int newXVelocity) {
+		this.xVelocity = newXVelocity;
+	}
+
+	public void setYVelocity(int newYVelocity) {
+		this.yVelocity = newYVelocity;
+	}
+
+	public void setX(int newX) {
+		this.xpos = newX;
+	}
+
+	public void setY(int newY) {
+		this.ypos = newY;
+	}
+
+	public void reverseX() {
+		this.xVelocity = -(this.yVelocity);
+	}
+
+	public void reverseY() {
+		this.yVelocity = -(this.yVelocity);
+	}
 }
--- workspace/Paddle.java
+++ workspace/Paddle.java
@@ -1,16 +1,64 @@
-import java.awt.Graphics;
 import java.awt.*;
 
 public class Paddle {
 
-	//your code here!
-	
-	//don't forget you need instance variables:
-	
-	
-	//constructor(s):
-	
-	//methods:
-	
-	
+	// your code here!
+
+	// don't forget you need instance variables:
+	private int x;
+	private int y;
+	private int width;
+	private int height;
+	private int velocity;
+
+	// constructor(s):
+	public Paddle(int initX, int initY, int initWidth, int initHeight) {
+		this.x = initX;
+		this.y = initY;
+		this.width = initWidth;
+		this.height = initHeight;
+		velocity = 1;
+	}
+
+	// methods:
+
+	public void draw(Graphics g) {
+		g.fillRect(x,y,width,height);
+	}
+
+	public void move() {
+		this.x += velocity;
+	}
+
+	public void addVelocity(int addV) {
+		this.velocity += addV;
+	}
+
+	public int getX() {
+		return x;
+	}
+
+	public int getY() {
+		return y;
+	}
+
+	public int getWidth() {
+		return width;
+	}
+
+	public int getHeight() {
+		return height;
+	}
+
+	public int getVelocity() {
+		return velocity;
+	}
+
+	public void setX(int newX) {
+		this.x = newX;
+	}
+
+	public void setVelocity(int newVelocity) {
+		this.velocity = newVelocity;
+	}
 }
